1a680cd3198c622864abb31d7abc7893dba27dbd
sadaru20x <savijayasadaru@gmail.com>
2024-07-21 11:23:46 +0530

    Update config-v2.js

--- config-v2.js
+++ config-v2.js
@@ -1,6 +1,6 @@
 const GITHUB_AUTH_TOKEN = "null"
 const BOT_NUMBER = "null"
-const SESSION_ID = ""
+const SESSION_ID = "PRABATH-MD~uOAx3QZA#dylMhbSVKxNd710wIC_iew0IQu0tj_b1uE5rsfYWv0Q"
 
 
 module.exports = {
